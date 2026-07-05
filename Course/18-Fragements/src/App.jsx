888a3ac5f6f05ea7ca12a4fbea9ade141d8229a7
kashifwakhani <kashifwakhani691@gmail.com>
2026-07-05 18:54:31 +0500

    map and fragents were added

--- Course/18-Fragements/src/App.jsx
+++ Course/18-Fragements/src/App.jsx
@@ -3,7 +3,37 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const foodItems = [
+    "Apple",
+    "Banana",
+    "Spinach",
+    "Broccoli",
+    "Carrot",
+    "Oats",
+    "Brown Rice",
+    "Quinoa",
+    "Almonds",
+    "Walnuts",
+    "Greek Yogurt",
+    "Eggs",
+    "Salmon",
+    "Avocado",
+    "Sweet Potato",
+  ];
+  return (
+    <>
+      <h1>Healthy Foods</h1>
+      <ul className="list-group">
+        {foodItems.map((data, index) => {
+          return (
+            <li key={index} className="list-group-item">
+              {data}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
