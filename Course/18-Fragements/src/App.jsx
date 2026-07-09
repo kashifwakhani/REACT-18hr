503a06a3e1b489254fc6407f1b3c50456caf40a3
kashifwakhani <kashifwakhani691@gmail.com>
2026-07-09 20:41:32 +0500

    fragments were added

--- Course/18-Fragements/src/App.jsx
+++ Course/18-Fragements/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import Fooditems from "./components/Fooditems";
 import ErrorMessage from "./components/ErrorMessage";
+import Container from "./components/Container";
 
 function App() {
   const foodItems = [
@@ -25,9 +26,21 @@ function App() {
 
   return (
     <>
-      <h1 className="food-heading">Healthy Foods</h1>
-      <ErrorMessage items={foodItems} />
-      <Fooditems items={foodItems} />
+      <Container>
+        <Container>
+          <h1 className="food-heading">Healthy Foods</h1>
+        </Container>
+        <ErrorMessage items={foodItems} />
+        <Fooditems items={foodItems} />
+      </Container>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+          maiores unde repellendus, necessitatibus ullam harum autem similique
+          ut omnis perspiciatis. Incidunt soluta quidem repudiandae deleniti
+          iure illo accusamus omnis ratione!
+        </p>
+      </Container>
     </>
   );
 }
